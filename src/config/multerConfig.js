3df0316089aa53c27efff36c6970348890f0aee6
eddev <eduardo.nog7@gmail.com>
2023-09-18 16:49:57 -0300

    tratando recebimento das imagens

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -4,6 +4,13 @@ import { extname, resolve } from 'path';
 const random = () => Math.floor(Math.random() * 10000 + 20000);
 
 export default {
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/jpg') {
+      return cb(new multer.MulterError('arquivo precisa ser png, jpg ou jpeg.'));
+    }
+
+    return cb(null, true);
+  },
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, resolve(__dirname, '..', '..', 'uploads'));
